b86f07fa34911247cfbbdbaf9dc051ec897715ce
zhongweihan@iqumeng.com <594309125@qq.com>
2017-10-13 15:30:19 +0800

    通用专题加号位置优化

--- modules/version/views/site/topic.php
+++ modules/version/views/site/topic.php
@@ -1369,7 +1369,7 @@ if($_SESSION['auth']=='1'){
                              '<li class="lit" >'+
 
                             '<img src="/file/3.png" style="position:relative;z-index:9999" alt="" onclick="addTop(this)" order="'+order+'" position="'+position+'">'+
-                                                        '<img style="position:absolute;top:30px;left:60px;width:30px;height:30px;border-radius:10px;" src="/file/u1892.png">'+
+                                                        '<img style="position:absolute;top:10px;left:60px;width:30px;height:30px;border-radius:10px;" src="/file/u1892.png">'+
                         '</li>'
                     );
                 }
