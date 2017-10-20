87933cd45745c50ec327b2430378324918d2e351
zhongweihan@iqumeng.com <594309125@qq.com>
2017-10-20 16:23:06 +0800

    屏幕数据记录审核过程

--- modules/version/controllers/ReviewController.php
+++ modules/version/controllers/ReviewController.php
@@ -512,7 +512,7 @@ class ReviewController extends VController
                 $allbtn=$_REQUEST['allbtn'];
                 if($allbtn=='已通过'){
                      $sql="select p.*,g.title as gtitle,s.name,c.username,b.add_time from yd_ver_screen_content_copy p inner join yd_ver_screen_guide g on p.screenGuideid=g.id and b.review_flag='1' inner join yd_ver_station s on s.id=g.gid left join yd_ver_review_record as b on p.id=b.bind_id left join yd_ver_admin as c on b.user_id=c.id";
-                     $sql = "select a.add_time,b.*,c.username,e.name from yd_ver_review_record as a inner join yd_ver_screen_content_copy as b on a.bind_id=b.id and a.review_flag='1' and a.type=3 inner join yd_ver_admin as c on a.user_id=c.id inner join yd_ver_screen_guide as d on b.screenGuideid=d.id inner join yd_ver_station e on e.id=d.gid";
+                     $sql = "select a.add_time,b.*,c.username,d.title as gtitle,e.name from yd_ver_review_record as a inner join yd_ver_screen_content_copy as b on a.bind_id=b.id and a.review_flag='1' and a.type=3 inner join yd_ver_admin as c on a.user_id=c.id inner join yd_ver_screen_guide as d on b.screenGuideid=d.id inner join yd_ver_station e on e.id=d.gid";
                       
                 }else if($allbtn=='已驳回'){
                      $sql="select p.*,g.title as gtitle,s.name,c.username,b.add_time from yd_ver_screen_content_copy p inner join yd_ver_screen_guide g on p.screenGuideid=g.id and b.review_flag='2' inner join yd_ver_station s on s.id=g.gid left join yd_ver_review_record as b on p.id=b.bind_id left join yd_ver_admin as c on b.user_id=c.id";
