3e3d04b556d60d3fb875f19eebc01c8f9a9407d3
zhongweihan@iqumeng.com <594309125@qq.com>
2017-11-03 15:25:35 +0800

    屏幕审核删除debug

--- modules/version/views/review/screenreview.php
+++ modules/version/views/review/screenreview.php
@@ -133,7 +133,7 @@ $adminLeftTwo = !empty($_GET['adminLeftTwo'])?$_GET['adminLeftTwo']:'';
                 ?>
                 <tr class="tr_list">
                     <td><input type="checkbox" name="id" value="<?php echo $v['id']?>"  <?php if(in_array($v['screenGuideid'],$copyGuides)){echo "disabled=disabled";}?> screenGuidid="<?=$v['screenGuideid']?>" onclick="checkQuote(this)" ></td>
-                    <td><?php echo date("Y-m-d h:i:s",$v['add_time'])?></td>
+                    <td><?php echo date("Y-m-d h:i",$v['add_time'])?></td>
 		    <td><?php echo $v['username'];?></td>
                     <td><?php
                             if($v['pic'] == '/file/3.png'){
